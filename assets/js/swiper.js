5df1d9b55d9e41c8aefa79993cb4266a21be777d
Vieirinhagui <vieiragui147@gmail.com>
2023-05-29 17:51:37 -0300

    att do home e imagens

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,21 +1,22 @@
-const swiper = new Swiper(".swiper", {  
-  
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    
-  });
-  const swiper1 = new Swiper(".swiperhome", {
-    slidesPerView: 2,
-    loop: true,
-    speed:7000,
-    disableOnInteraction:false,
-    autoplay: {
-      delay: 1000,
-    },
-  });
-  
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const swiper1 = new Swiper(".swiperhome", {
+  slidesPerView: 2,
+  loop: true,
+  speed: 5000,
+  disableOnInteraction: false,
+  autoplay: {
+    delay: 3000,
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+});
